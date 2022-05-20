70b630720d2dc232f9b56c0ca9a2aa9d90780327
Kingzheimuzi <102216145+Kingzheimuzi@users.noreply.github.com>
2022-05-20 23:38:31 +0800

    Update IntNode.h

--- include/IntNode.h
+++ include/IntNode.h
@@ -13,15 +13,23 @@ class IntNode{
     //int ofcap; // capacity for overflow block
     int maincnt; // real size of main block, <=maincap
     int ofcnt; // real size of overflow block, <=ofcap
+    int tombcnt;
+
+    const int maincap = 10; // capacity for main block
+    const int ofcap = 3; // capacity for overflow block
+    const int mergefactor=8;    //If less than 1/n in a block, merge with the right one 
+
     IntNode* leftNeighbor;
     IntNode* rightNeighbor;
+    void Adjustof();
+    int Num();
     void insertB(MeTPerson* tmp);
+    int Find(MeTPerson* tmp);   //-2: found in of  -1:not found  otherwise:found in main, return index
     void deleteB(MeTPerson* tmp);
     MeTPerson* topPer();
     
-    private:
-    vector<MeTPerson*> mainB;
-    vector<MeTPerson*> ofB;
+    MeTPerson* mainB[13];
+    MeTPerson* ofB[5];
     void mergeB();
     void splitB();
     void sortB();
