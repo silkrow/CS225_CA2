c69900838daac70198d37828ed273cd5b286e6b5
Erikaqvq <85092199+Erikaqvq@users.noreply.github.com>
2022-05-21 15:49:32 +0800

    Add files via upload

---
+++ src/BPlusTree.h
@@ -0,0 +1,40 @@
+#ifndef BP_TREE
+#define BP_TREE
+#include "PersonDB.h"
+//#include "MeTPerson.h"
+#include "BPNode.h"
+#define order 4
+#define LEAF true
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class MeTPerson;
+
+//template<typename MeTPerson*, typename MeTPerson*>
+class BPTree{
+private:
+    Node *root;
+    inline int keyIndex(Node *_node, MeTPerson* _key);
+    inline int eqkeyIndex(Node *_node, MeTPerson* _key);
+    inline pair<Node*, int> keyIndexInLeaf(MeTPerson* _key);
+    inline pair<Node*, int> eqkeyIndexInLeaf(MeTPerson* _key);
+    Node* splitLeaf(Node* _leaf);
+    void createIndex(Node* _new_node, MeTPerson* _index);
+    pair<Node*, MeTPerson*> splitNode(Node* _node);
+
+public:
+    BPTree();
+    int sz;
+    void insert(MeTPerson* _key);
+    void erase(MeTPerson* _key);
+
+    MeTPerson* find(MeTPerson* _key);
+    void display();
+    void scan();
+    PerDB* PDB;
+    MeTPerson* poptop(int num);
+};
+
+#endif
